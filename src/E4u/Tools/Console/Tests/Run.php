f9648fb05a125ab0a2c97bef64ca95b4ec48fb2f
Artur Jedliński <artur@jedlinski.pl>
2018-09-14 10:36:14 +0200

    fix tests:run

--- src/E4u/Tools/Console/Tests/Run.php
+++ src/E4u/Tools/Console/Tests/Run.php
@@ -18,19 +18,21 @@ class Run extends Base
             return false;
         }
 
-        if (!class_exists($srcClass)) {
-            echo sprintf("ERROR: Class %s not found.\n", $srcClass);
-            return false;
-        }
-
         $rootNamespace = strtok($srcClass, '\\');
         $testClass = $rootNamespace . 'Test' . substr($srcClass, strlen($rootNamespace)) . 'Test';
 
-        $testFile = 'tests' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $testClass) . '.php';
-        if (!file_exists($testFile)) {
-            echo sprintf("ERROR: File %s not found.\n", $testFile);
+        if (!class_exists($srcClass)) {
+            echo sprintf("ERROR: Class %s not found.\nMake sure autoload for %s\\ namespace is defined in composer.json.\n", $srcClass, $rootNamespace);
             return false;
         }
+        
+        if (!class_exists($testClass)) {
+            echo sprintf("ERROR: Test class %s not found.\nMake sure autoload for %s\\ namespace is defined in composer.json.\n", $srcClass, $rootNamespace . 'Test');
+            return false;
+        }
+
+        $reflector = new \ReflectionClass($testClass);
+        $testFile = $reflector->getFileName();
 
         $command = "phpunit --bootstrap tests\\bootstrap.php $testFile";
         system($command);
